c3d173e7adda22c819e3d770919056f23ed1fd85
Learning Horizons Education Society <115077457+Learning-Horizons-Education-Society@users.noreply.github.com>
2022-10-06 16:06:59 +0530

    Create mail.js

---
+++ js/mail.js
@@ -0,0 +1,11 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyBl_Di3v919AzZa5JYcbV2_94mJFA7F-4s",
+    authDomain: "learning-horizons-shool.firebaseapp.com",
+    databaseURL: "https://learning-horizons-shool-default-rtdb.firebaseio.com",
+    projectId: "learning-horizons-shool",
+    storageBucket: "learning-horizons-shool.appspot.com",
+    messagingSenderId: "93337375144",
+    appId: "1:93337375144:web:e34227f3a0ce57df7d8d9d"
+  };
+
+firebase.initializeApp(firebaseConfig);
